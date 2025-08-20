31c254d40c4e61ddbb343a27e2afabb479349060
rober122pst <jrobertinhor.9@gmail.com>
2025-08-20 16:55:03 -0300

    commit

--- main.py
+++ main.py
@@ -81,26 +81,49 @@ def calc_team_overall(team_id: int) -> tuple[str, int]:
     return team['nome'], over_av
 
 def simular_partida(team_id_home, team_id_away):
+    goal_home = 0
+    goal_away = 0
+
     team_home = find_team_by_id(team_id_home)
     team_away = find_team_by_id(team_id_away)
-    team_home_over = calc_team_overall(team_id_home)
-    team_away_over = calc_team_overall(team_id_away)
-        
-    diff = int(((min(team_home_over, team_away_over)/2) / max(team_home_over, team_away_over))*100)
-    
-    if team_home_over > team_away_over:
-        home_chance = 100 - diff
-        away_chance = diff
-    elif team_away_over > team_home_over:
-        away_chance = 100 - diff
-        home_chance = diff
-    else:
-        away_chance = diff
-        home_chance = diff
-        
+    _, team_home_overall = calc_team_overall(team_id_home)
+    _, team_away_overall = calc_team_overall(team_id_away)
+
+    diff = abs(team_home_overall - team_away_overall)
+    over_av = (team_home_overall + team_away_overall) // 2
+
+    random_goal_chance = 150 - (diff*1.5) - (over_av*.5)
+
+    if random_goal_chance < 1:
+        random_goal_chance = 1
     
     for time in range(1, 91):
-        rand = random.randint(0, 100)
-        if 
+        if random.randint(1, int(random_goal_chance)) == 1:
+            probability_home = team_home_overall
+            probability_away = team_away_overall
+
+            goal_choice = random.choices(
+                [team_home['nome'], team_away['nome']],
+                weights=[probability_home, probability_away],
+                k=1
+            )[0]
+        
+            if goal_choice == team_home['nome']:
+                goal_home += 1
+            else:
+                goal_away += 1
+
+    resultado = {
+        "placar": {
+            f"{team_home['nome']}": goal_home,
+            f"{team_away['nome']}": goal_away
+        }
+    }
+
+    return resultado
+for i in range(50):
+    resultado = simular_partida(1, 2)
+    print("Placar:", resultado["placar"])
+
     
 
